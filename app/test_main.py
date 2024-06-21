c789f151eda2437c9ebaa43a24033dd627f3ea8f
Danylo Minkov <minkov215@gmail.com>
2024-06-21 11:40:37 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize("cents, expected", [
+    (1, [1, 0, 0, 0]),   # 1 penny
+    (6, [1, 1, 0, 0]),   # 1 penny + 1 nickel
+    (17, [2, 1, 1, 0]),  # 2 pennies + 1 nickel + 1 dime
+    (50, [0, 0, 0, 2]),  # 2 quarters
+    (0, [0, 0, 0, 0]),   # 0 cents
+    (99, [4, 0, 2, 3]),  # 4 pennies + 2 dimes + 3 quarters
+    (100, [0, 0, 0, 4]), # 4 quarters
+    (41, [1, 1, 1, 1]),  # 1 penny + 1 nickel + 1 dime + 1 quarter
+    (63, [3, 0, 1, 2]),  # 3 pennies + 1 dime + 2 quarters
+])
+def test_get_coin_combination(cents: int, expected: list) -> None:
+    assert get_coin_combination(cents) == expected
